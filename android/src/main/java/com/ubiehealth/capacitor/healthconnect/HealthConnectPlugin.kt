5b296b814fd5ab3b48f9f9671c32f8bf98eb8757
Eduard Strehlau <eduard@lionizers.com>
2024-08-23 14:59:12 +0200

    Notify client of exceptions (time of check, time of use) for permissions WILL happen

--- android/src/main/java/com/ubiehealth/capacitor/healthconnect/HealthConnectPlugin.kt
+++ android/src/main/java/com/ubiehealth/capacitor/healthconnect/HealthConnectPlugin.kt
@@ -35,6 +35,7 @@ class HealthConnectPlugin : Plugin() {
             HealthConnectClient.SDK_AVAILABLE -> "Available"
             HealthConnectClient.SDK_UNAVAILABLE -> "NotSupported"
             HealthConnectClient.SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED -> "NotInstalled"
+            // Impossible, ok to crash
             else -> throw RuntimeException("Invalid sdk status: $status")
         }
 
@@ -61,109 +62,132 @@ class HealthConnectPlugin : Plugin() {
     @PluginMethod
     fun insertRecords(call: PluginCall) {
         this.activity.lifecycleScope.launch {
-            val records = call.getArray("records").toList<JSONObject>().map { it.toRecord() }
-            val result = healthConnectClient.insertRecords(records)
+            try {
+                val records = call.getArray("records").toList<JSONObject>().map { it.toRecord() }
+                val result = healthConnectClient.insertRecords(records)
 
-            val res = JSObject().apply {
-                put("recordIds", result.recordIdsList)
+                val res = JSObject().apply {
+                    put("recordIds", result.recordIdsList)
+                }
+                call.resolve(res)
+            } catch (e: Exception) {
+                call.reject("error inserting record", e);
             }
-            call.resolve(res)
         }
     }
 
     @PluginMethod
     fun readRecord(call: PluginCall) {
         this.activity.lifecycleScope.launch {
-            val type = call.getString("type").let {
-                RECORDS_TYPE_NAME_MAP[it] ?: throw IllegalArgumentException("Unexpected RecordType: $it")
-            }
+            try {
+                val type = call.getString("type").let {
+                    RECORDS_TYPE_NAME_MAP[it]
+                        ?: throw IllegalArgumentException("Unexpected RecordType: $it")
+                }
 
-            val result = healthConnectClient.readRecord(
-                    recordType = type,
-                    recordId = requireNotNull(call.getString("recordId"))
-            )
+                val result = healthConnectClient.readRecord(
+                    recordType = type, recordId = requireNotNull(call.getString("recordId"))
+                )
 
-            val res = JSObject().apply {
-                this.put("record", result.record.toJSONObject())
+                val res = JSObject().apply {
+                    this.put("record", result.record.toJSONObject())
+                }
+                call.resolve(res)
+            } catch (e: Exception) {
+                call.reject("error reading record", e);
             }
-            call.resolve(res)
         }
     }
 
     @PluginMethod
     fun readRecords(call: PluginCall) {
         this.activity.lifecycleScope.launch {
-            val type = call.getString("type").let {
-                RECORDS_TYPE_NAME_MAP[it] ?: throw IllegalArgumentException("Unexpected RecordType: $it")
-            }
-            val request = ReadRecordsRequest(
+            try {
+                val type = call.getString("type").let {
+                    RECORDS_TYPE_NAME_MAP[it]
+                        ?: throw IllegalArgumentException("Unexpected RecordType: $it")
+                }
+                val request = ReadRecordsRequest(
                     recordType = type,
                     timeRangeFilter = call.data.getTimeRangeFilter("timeRangeFilter"),
                     dataOriginFilter = call.data.getDataOriginFilter("dataOriginFilter"),
                     ascendingOrder = call.getBoolean("ascendingOrder") ?: true,
                     pageSize = call.getInt("pageSize") ?: 1000,
                     pageToken = call.getString("pageToken"),
-            )
-            val result = healthConnectClient.readRecords(request)
+                )
+                val result = healthConnectClient.readRecords(request)
 
-            val res = JSObject().apply {
-                val records = result.records.map { it.toJSONObject() }.toJSONArray()
-                this.put("records", records)
-                this.put("pageToken", result.pageToken)
+                val res = JSObject().apply {
+                    val records = result.records.map { it.toJSONObject() }.toJSONArray()
+                    this.put("records", records)
+                    this.put("pageToken", result.pageToken)
+                }
+                call.resolve(res)
+            } catch (e: Exception) {
+                call.reject("error reading record", e);
             }
-            call.resolve(res)
         }
     }
 
     @PluginMethod
     fun getChangesToken(call: PluginCall) {
         this.activity.lifecycleScope.launch {
-            val types = call.getArray("types").toList<String>().map {
-                RECORDS_TYPE_NAME_MAP[it] ?: throw IllegalArgumentException("Unexpected RecordType: $it")
-            }.toSet()
-            val request = ChangesTokenRequest(
+            try {
+                val types = call.getArray("types").toList<String>().map {
+                    RECORDS_TYPE_NAME_MAP[it]
+                        ?: throw IllegalArgumentException("Unexpected RecordType: $it")
+                }.toSet()
+                val request = ChangesTokenRequest(
                     recordTypes = types,
                     dataOriginFilters = call.data.getDataOriginFilter("dataOriginFilter"),
-            )
-            val token = healthConnectClient.getChangesToken(request)
+                )
+                val token = healthConnectClient.getChangesToken(request)
 
-            val res = JSObject().apply {
-                this.put("token", token)
+                val res = JSObject().apply {
+                    this.put("token", token)
+                }
+                call.resolve(res)
+            } catch (e: Exception) {
+                call.reject("error getting change token", e);
+
             }
-            call.resolve(res)
         }
     }
+
 
     @PluginMethod
     fun getChanges(call: PluginCall) {
         this.activity.lifecycleScope.launch {
-            var token = requireNotNull(call.getString("token"))
-            val changes = flow {
-                do {
-                    val result = healthConnectClient.getChanges(
+            try {
+                var token = requireNotNull(call.getString("token"))
+                val changes = flow {
+                    do {
+                        val result = healthConnectClient.getChanges(
                             changesToken = token,
-                    )
-                    emit(result.changes)
-                    token = result.nextChangesToken
-                } while (result.hasMore)
-            }.toList().flatten()
+                        )
+                        emit(result.changes)
+                        token = result.nextChangesToken
+                    } while (result.hasMore)
+                }.toList().flatten()
 
-            val res = JSObject().apply {
-                put("changes", changes.map { it.toJSObject() }.toJSONArray())
-                put("nextToken", token)
+                val res = JSObject().apply {
+                    put("changes", changes.map { it.toJSObject() }.toJSONArray())
+                    put("nextToken", token)
+                }
+                call.resolve(res)
+            } catch (e: Exception) {
+                call.reject("error getting changes", e)
+
             }
-            call.resolve(res)
         }
     }
 
     private fun installationIntent(): Intent {
         val intent = Intent(Intent.ACTION_VIEW)
         intent.setPackage("com.android.vending")
-        intent.data = Uri.parse("market://details")
-            .buildUpon()
+        intent.data = Uri.parse("market://details").buildUpon()
             .appendQueryParameter("id", "com.google.android.apps.healthdata")
-            .appendQueryParameter("url", "healthconnect://onboarding")
-            .build()
+            .appendQueryParameter("url", "healthconnect://onboarding").build()
         intent.putExtra("overlay", true)
         intent.putExtra("callerId", context.packageName)
         return intent
@@ -171,39 +195,44 @@ class HealthConnectPlugin : Plugin() {
 
     @PluginMethod
     fun requestHealthPermissions(call: PluginCall) {
-        val sdkStatus = HealthConnectClient.getSdkStatus(this.context)
-        if (sdkStatus == HealthConnectClient.SDK_UNAVAILABLE) {
-            val res = JSObject().apply {
-                put("grantedPermissions", JSArray())
-                put("hasAllPermissions", false)
+        try {
+            val sdkStatus = HealthConnectClient.getSdkStatus(this.context)
+            if (sdkStatus == HealthConnectClient.SDK_UNAVAILABLE) {
+                val res = JSObject().apply {
+                    put("grantedPermissions", JSArray())
+                    put("hasAllPermissions", false)
+                }
+                call.resolve(res)
+                return
             }
-            call.resolve(res)
-            return
-        }
 
-        if (sdkStatus == HealthConnectClient.SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED) {
-            val intent = installationIntent()
-            startActivityForResult(call, intent, "handleInstalled")
-            return
-        }
+            if (sdkStatus == HealthConnectClient.SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED) {
+                val intent = installationIntent()
+                startActivityForResult(call, intent, "handleInstalled")
+                return
+            }
 
-        val readPermissions = call.getArray("read").toList<String>().map {
-            HealthPermission.getReadPermission(
-                recordType = RECORDS_TYPE_NAME_MAP[it] ?: throw IllegalArgumentException("Unexpected RecordType: $it")
+            val readPermissions = call.getArray("read").toList<String>().map {
+                HealthPermission.getReadPermission(
+                    recordType = RECORDS_TYPE_NAME_MAP[it]
+                        ?: throw IllegalArgumentException("Unexpected RecordType: $it")
+                )
+            }.toSet()
+            val writePermissions = call.getArray("write").toList<String>().map {
+                HealthPermission.getWritePermission(
+                    recordType = RECORDS_TYPE_NAME_MAP[it]
+                        ?: throw IllegalArgumentException("Unexpected RecordType: $it")
+                )
+            }.toSet()
+
+            val intent = permissionContract.createIntent(
+                this.context, readPermissions + writePermissions
             )
-        }.toSet()
-        val writePermissions = call.getArray("write").toList<String>().map {
-            HealthPermission.getWritePermission(
-                recordType = RECORDS_TYPE_NAME_MAP[it] ?: throw IllegalArgumentException("Unexpected RecordType: $it")
-            )
-        }.toSet()
 
-        val intent = permissionContract.createIntent(
-            this.context,
-            readPermissions + writePermissions
-        )
-
-        startActivityForResult(call, intent, "handleRequestPermission")
+            startActivityForResult(call, intent, "handleRequestPermission")
+        } catch (e: Exception) {
+            call.reject("error requesting permissions", e);
+        }
     }
 
     @ActivityCallback
@@ -222,69 +251,99 @@ class HealthConnectPlugin : Plugin() {
 
     @ActivityCallback
     fun handleRequestPermission(call: PluginCall, result: ActivityResult) {
-        val reqReadPermissions = call.getArray("read").toList<String>().associateBy {
-            HealthPermission.getReadPermission(
-                    recordType = RECORDS_TYPE_NAME_MAP[it] ?: throw IllegalArgumentException("Unexpected RecordType: $it")
-            )
-        }
-        val reqWritePermissions = call.getArray("write").toList<String>().associateBy {
-            HealthPermission.getWritePermission(
-                    recordType = RECORDS_TYPE_NAME_MAP[it] ?: throw IllegalArgumentException("Unexpected RecordType: $it")
-            )
-        }
+        try {
+            val reqReadPermissions = call.getArray("read").toList<String>().associateBy {
+                HealthPermission.getReadPermission(
+                    recordType = RECORDS_TYPE_NAME_MAP[it]
+                        ?: throw IllegalArgumentException("Unexpected RecordType: $it")
+                )
+            }
+            val reqWritePermissions = call.getArray("write").toList<String>().associateBy {
+                HealthPermission.getWritePermission(
+                    recordType = RECORDS_TYPE_NAME_MAP[it]
+                        ?: throw IllegalArgumentException("Unexpected RecordType: $it")
+                )
+            }
 
-        val grantedPermissions = permissionContract.parseResult(result.resultCode, result.data).toSet()
-        val hasAllPermissions = grantedPermissions.containsAll(reqReadPermissions.keys + reqWritePermissions.keys)
+            val grantedPermissions =
+                permissionContract.parseResult(result.resultCode, result.data).toSet()
+            val hasAllPermissions =
+                grantedPermissions.containsAll(reqReadPermissions.keys + reqWritePermissions.keys)
 
-        val grantedPermissionsResult = JSObject().apply {
-            put("read", JSArray(reqReadPermissions.filterKeys { grantedPermissions.contains(it) }.values))
-            put("write", JSArray(reqWritePermissions.filterKeys { grantedPermissions.contains(it) }.values))
-        }
+            val grantedPermissionsResult = JSObject().apply {
+                put(
+                    "read",
+                    JSArray(reqReadPermissions.filterKeys { grantedPermissions.contains(it) }.values)
+                )
+                put(
+                    "write",
+                    JSArray(reqWritePermissions.filterKeys { grantedPermissions.contains(it) }.values)
+                )
+            }
 
-        val res = JSObject().apply {
-            put("grantedPermissions", grantedPermissionsResult)
-            put("hasAllPermissions", hasAllPermissions)
+            val res = JSObject().apply {
+                put("grantedPermissions", grantedPermissionsResult)
+                put("hasAllPermissions", hasAllPermissions)
+            }
+            call.resolve(res)
+        } catch (e: Exception) {
+            call.reject("error requesting permission", e)
         }
-        call.resolve(res)
     }
 
     @PluginMethod
     fun checkHealthPermissions(call: PluginCall) {
         this.activity.lifecycleScope.launch {
-            val reqReadPermissions = call.getArray("read").toList<String>().associateBy {
-                HealthPermission.getReadPermission(
+            try {
+                val reqReadPermissions = call.getArray("read").toList<String>().associateBy {
+                    HealthPermission.getReadPermission(
                         recordType = RECORDS_TYPE_NAME_MAP[it]
-                                ?: throw IllegalArgumentException("Unexpected RecordType: $it")
-                )
-            }
-            val reqWritePermissions = call.getArray("write").toList<String>().associateBy {
-                HealthPermission.getWritePermission(
+                            ?: throw IllegalArgumentException("Unexpected RecordType: $it")
+                    )
+                }
+                val reqWritePermissions = call.getArray("write").toList<String>().associateBy {
+                    HealthPermission.getWritePermission(
                         recordType = RECORDS_TYPE_NAME_MAP[it]
-                                ?: throw IllegalArgumentException("Unexpected RecordType: $it")
-                )
-            }
+                            ?: throw IllegalArgumentException("Unexpected RecordType: $it")
+                    )
+                }
 
-            val grantedPermissions = healthConnectClient.permissionController.getGrantedPermissions()
-            val hasAllPermissions = grantedPermissions.containsAll(reqReadPermissions.keys + reqWritePermissions.keys)
+                val grantedPermissions =
+                    healthConnectClient.permissionController.getGrantedPermissions()
+                val hasAllPermissions =
+                    grantedPermissions.containsAll(reqReadPermissions.keys + reqWritePermissions.keys)
 
-            val grantedPermissionsResult = JSObject().apply {
-                put("read", JSArray(reqReadPermissions.filterKeys { grantedPermissions.contains(it) }.values))
-                put("write", JSArray(reqWritePermissions.filterKeys { grantedPermissions.contains(it) }.values))
-            }
+                val grantedPermissionsResult = JSObject().apply {
+                    put(
+                        "read",
+                        JSArray(reqReadPermissions.filterKeys { grantedPermissions.contains(it) }.values)
+                    )
+                    put(
+                        "write",
+                        JSArray(reqWritePermissions.filterKeys { grantedPermissions.contains(it) }.values)
+                    )
+                }
 
-            val res = JSObject().apply {
-                put("grantedPermissions",  grantedPermissionsResult)
-                put("hasAllPermissions", hasAllPermissions)
+                val res = JSObject().apply {
+                    put("grantedPermissions", grantedPermissionsResult)
+                    put("hasAllPermissions", hasAllPermissions)
+                }
+                call.resolve(res)
+            } catch (e: Exception) {
+                call.reject("error checking permissions", e)
             }
-            call.resolve(res)
         }
     }
 
     @PluginMethod
     fun revokeHealthPermissions(call: PluginCall) {
         this.activity.lifecycleScope.launch {
-            healthConnectClient.permissionController.revokeAllPermissions()
-            call.resolve()
+            try {
+                healthConnectClient.permissionController.revokeAllPermissions()
+                call.resolve()
+            } catch (e: Exception) {
+                call.reject("error revoking permissions", e)
+            }
         }
     }
 
